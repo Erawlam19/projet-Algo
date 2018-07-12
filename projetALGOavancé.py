0d3df6b0b8a1436ee8e8a82ec5b639477a585809
Erawlam19 <richiardi.paul@gmail.com>
2018-07-12 19:50:56 +0200

    Ajout Tri + maj

--- projetALGOavancé.py
+++ projetALGOavancé.py
@@ -36,7 +36,7 @@ class Noeud:
         else:
             self.valeur = Noeud.valeur
 
-    #Affiche l'arbre en parcours Infixe
+    # Affiche l'arbre en parcours Infixe
     def afficherInfixe(self):
         if self.filsGauche:
             self.filsGauche.afficherInfixe()
@@ -44,7 +44,7 @@ class Noeud:
         if self.filsDroit:
             self.filsDroit.afficherInfixe()
 
-    #Affiche l'arbre en parcours Postfixe
+    # Affiche l'arbre en parcours Postfixe
     def afficherPostfixe(self):
         if self.filsGauche:
             self.filsGauche.afficherPostfixe()
@@ -52,28 +52,25 @@ class Noeud:
             self.filsDroit.afficherPostfixe()
         print(self.valeur,";", end='')
 
-#A traduir...
+# Supprime le noeud d'un arbre
 def deleteNode(root, key):
  
-    # Base Case
+    # Si le noeud courant est la racine
     if root is None:
         return root 
  
-    # If the key to be deleted is smaller than the root's
-    # key then it lies in  left subtree
+    # Si le noeud à supprimer est plus petit que la racine, on va dans le sous-arbre gauche
     if key < root.valeur:
         root.filsGauche = deleteNode(root.filsGauche, key)
  
-    # If the kye to be delete is greater than the root's key
-    # then it lies in right subtree
+    # Si le noeud à supprimer est plus petit que la racine, on va dans le sous-arbre droit
     elif(key > root.valeur):
         root.filsDroit = deleteNode(root.filsDroit, key)
  
-    # If key is same as root's key, then this is the node
-    # to be deleted
+    # Si le noeud est le même que le noeud principal, alors c'est le noeud à supprimer
     else:
          
-        # Node with only one child or no child
+        # Noeud avec un seul enfant ou aucun enfant
         if root.filsGauche is None :
             temp = root.filsDroit 
             root = None
@@ -84,23 +81,50 @@ def deleteNode(root, key):
             root = None
             return temp
  
-        # Node with two children: Get the inorder successor
-        # (smallest in the right subtree)
+        # Noeud avec deux enfants: on prend le plus petit dans le sous-arbre droit
         temp = minValueNode(root.filsDroit)
  
-        # Copy the inorder successor's content to this node
+        # Copier le successeur infixe du noeud
         root.valeur = temp.valeur
  
-        # Delete the inorder successor
+        # Supprimer le successeur infixe
         root.filsDroit = deleteNode(root.filsDroit , temp.valeur)
  
  
-    return root 
+    return root
+
+## Fonction pour convertir l'INPUT (= une liste representant les noeuds) en un ABR équilibré
+def TriListeABR(arr):
+     
+    if not arr:
+        return None
+ 
+    # Pour trouver le noeud du milieu dans la liste
+    mid = (len(arr)) // 2
+     
+    # Prendre le noeud du milieu en tant que racine
+    root = Noeud(arr[mid])
+     
+    # Sous arbre gauche de la racine par selon les valeurs
+    root.filsGauche = TriListeABR(arr[:mid])
+     
+    # Sous arbre droit de la racine par selon les valeurs
+    root.filsDroit = TriListeABR(arr[mid+1:])
+    return root
+
+## Permet d'afficher larbre apres Tri 
+def AffichageABRTri(Noeud):
+	if not Noeud:
+		return	
+	print (Noeud.valeur),
+	AffichageABRTri(Noeud.filsGauche)
+	AffichageABRTri(Noeud.filsDroit)
+	
 #---------------------FIN DE CLASSE---------------------#
 
-#Ecrit l'arbre dans un fichier csv ('test.csv', delimiter = ';'),
-#le fichier doit être vide au préalable, utiliser "ClearFile()" pour ça.        
-#Il faut aussi supprimer le dernier ';', utiliser "DeleteLastChar()" pour ça. 
+## Ecrit l'arbre dans un fichier csv ('test.csv', delimiter = ';'),
+# le fichier doit être vide au préalable, utiliser "ClearFile()" pour ça.        
+# Il faut aussi supprimer le dernier ';', utiliser "DeleteLastChar()" pour ça. 
 def ecrireArbreFichier(Noeud):
     myfile = open("test.csv", "a")
     myfile.write(str(Noeud.valeur) + ";")
@@ -110,7 +134,7 @@ def ecrireArbreFichier(Noeud):
     if Noeud.filsDroit:
         ecrireArbreFichier(Noeud.filsDroit)
         
-#Importe un arbre depuis un fichier csv ('test.csv', delimiter = ';'),
+## Importe un arbre depuis un fichier csv ('test.csv', delimiter = ';'),
 def LireArbreFichier():
         listeNoeud = FirstCSVRowTOList()
         unNoeud = Noeud(0)
@@ -124,14 +148,14 @@ def LireArbreFichier():
                 unNoeud.insert(tempNoeud)
         return unNoeud
 
-#Retourne la premiere ligne(=row) du fichier 'test.csv' sous forme de liste
+## Retourne la premiere ligne(=row) du fichier 'test.csv' sous forme de liste
 def FirstCSVRowTOList():
     with open('test.csv', 'r') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=';')
         for row in spamreader:
             return row
 
-#Supprime le dernier caractère d'un fichier        
+## Supprime le dernier caractère d'un fichier        
 def DeleteLastChar():
     with open("test.csv", "r") as f:
         file_str = str(f.read())
@@ -141,11 +165,11 @@ def DeleteLastChar():
         f.write(file_str)
     f.close()
 
-#Vide le contenu d'un fichier
+## Vide le contenu d'un fichier
 def ClearFile():
     open("test.csv", 'w').close()
 
-#retourne la taille de l'arbre
+## Retourne la taille de l'arbre
 def tailleArbre(Noeud):
     if Noeud is None:
         return 0
@@ -153,7 +177,7 @@ def tailleArbre(Noeud):
         return (tailleArbre(Noeud.filsGauche)+ 1 + tailleArbre(Noeud.filsDroit))
       
 
-#Calcule la profondeur MAX "maxProfondeur" d'un arbre sleon le nombre de noeuds -
+## Calcule la profondeur MAX "maxProfondeur" d'un arbre sleon le nombre de noeuds -
 #Le chemin le plus long depuis le noeud racine jusqu'au noeud le plus éloigné
 
 def maxProfondeur(self):
@@ -162,18 +186,18 @@ def maxProfondeur(self):
  
     else :
  
-        # Compute the Profondeur of each subtree
+        # Calcul la profondeur de chaque sous arbre
         GProfondeur = maxProfondeur(self.filsGauche)
         DProfondeur = maxProfondeur(self.filsDroit)
  
-        # Use the larger one
+        # Prendre la plus grande largeur
         if (GProfondeur > DProfondeur):
             return GProfondeur+1
         else:
             return DProfondeur+1
  
 
-#Penser à rajouter un return, si la valeur n'est pas trouvée
+## Permet de rechercher un NOEUD ##
 def search(Noeud,key):
 
     if(Noeud is None):
@@ -191,7 +215,7 @@ def search(Noeud,key):
         return search(Noeud.filsDroit,key)
 
 
-
+##################
     
 monNoeud = Noeud(25) #Déclaration d'un premier Noeud
 
@@ -203,10 +227,10 @@ monNoeud.insert(Noeud(3))
 monNoeud.insert(Noeud(2))
 monNoeud.insert(Noeud(1))
 
-print("Affichage infixe: ")
+print("Affichage parcours infixe: ")
 monNoeud.afficherInfixe()
 print("\n")
-print("Affichage postefixe: ")
+print("Affichage parcours postefixe: ")
 monNoeud.afficherPostfixe()
 print("\n")
 print("Export de l'arbre vers test.csv")
@@ -215,18 +239,25 @@ ecrireArbreFichier(monNoeud)
 DeleteLastChar()
 print("Import de l'arbre depuis test.csv")
 unNoeud = LireArbreFichier()
-print("Affichage infixe du Noeud importé: ")
+print("\n")
+print("Affichage parcours infixe du Noeud importé: ")
 unNoeud.afficherInfixe()
-print("Affichage postfixe du Noeud importé: ")
+print("\n")
+print("Affichage parcours postfixe du Noeud importé: ")
 unNoeud.afficherPostfixe()
-
-
+print("\n")
 
 print ("La taille de larbre est de %d" %(tailleArbre(monNoeud)))
 print ("La hauteur de larbre est %d" %(maxProfondeur(monNoeud)))
 print (search(monNoeud,23))
 
-print("Affichage postfixe du Noeud apres suppresion de 15: ")
+print("Affichage parcours postfixe apres suppresion de 15: ")
 unNoeud = deleteNode(unNoeud, 15)
 unNoeud.afficherPostfixe()
+print("\n")
 
+print ("On saisi les noeuds suivants: 1, 2, 3, 4, 5, 6")
+arr = [1, 2, 3, 4, 5, 6]
+root = TriListeABR(arr)
+print ("ABR triée:")
+AffichageABRTri(root)
